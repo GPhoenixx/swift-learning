0f0e3e69be2fb21f3703a5718268057d151f0732
thuong.dang <thuong.dang@mservice.com.vn>
2024-09-28 12:50:21 +0700

    Initial Commit

---
+++ swift-learning/swift_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_learningApp.swift
+//  swift-learning
+//
+//  Created by thuong.dang on 28/9/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
